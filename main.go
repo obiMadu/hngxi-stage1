80508d9785dacc5506c58dcdd33084b03c92663b
Obi.M <mail@obimadu.pro>
2024-07-01 13:17:27 +0100

    feat: implement endpoint

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"strings"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
-// define a webPort constant
+// define constants
 const webPort = ":8082"
+const ipInfoURL = "https://ipinfo.io"
+const ipWeatherURL = "https://api.openweathermap.org/data/2.5/weather"
+
+type ipLocInfo struct {
+	City string `json:"city"`
+	Loc  string `json:"loc"`
+}
+
+type ipWeatherInfo struct {
+	Main struct {
+		Temp string `json:"temp"`
+	} `json:"main"`
+}
 
 func main() {
 	if err := godotenv.Load(); err != nil {
@@ -18,11 +36,99 @@ func main() {
 
 	// make a gin router
 	mux := gin.Default()
+
 	// use cors
 	mux.Use(cors.Default())
+
 	// setup route
 	mux.GET("/api/hello", func(c *gin.Context) {
+		// get client IP
+		ip := c.ClientIP()
 
+		// get visitor_name query
+		visitor_name := strings.Trim(c.Query("visitor_name"), "\"")
+		fmt.Println(visitor_name)
+		if visitor_name == "" {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status":  "error",
+				"message": "you did not provide a visitor_name query",
+			})
+			return
+		}
+
+		// get IP location details from ipinfo.io
+		var ipLocInfo ipLocInfo
+		url := fmt.Sprintf("%s/%s/json", ipInfoURL, ip)
+
+		// call for IP location info
+		resp, err := http.Get(url)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"status":  "error",
+				"message": "failed to get ip location info from ipinfo.io",
+			})
+			return
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode == 200 {
+			err := json.NewDecoder(resp.Body).Decode(&ipLocInfo)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"status":  "error",
+					"message": "failed to process ip location info from ipinfo.io",
+				})
+				return
+			}
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"status":  "error",
+				"message": "failed to get ip location info from ipinfo.io",
+			})
+			return
+		}
+
+		// get temperation info from OpenWeatherMap
+		// get latitude and longitude
+		var lat, lon string
+		fmt.Sscanf(ipLocInfo.Loc, "%f,%f", &lat, &lon)
+
+		// get api key from environment
+		apiKey := os.Getenv("OPENWEATHERMAP_APIKEY")
+
+		// call OpenWeatherMap
+		url = fmt.Sprintf("%s?lat=%s&lon=%s&appid=%s&units=metric", ipWeatherURL, lat, lon, apiKey)
+		resp, err = http.Get(url)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"status":  "error",
+				"message": "failed to contact ip weather service",
+			})
+			return
+		}
+
+		var ipWeatherInfo ipWeatherInfo
+		if resp.StatusCode != 200 {
+			if err := json.NewDecoder(resp.Body).Decode(&ipWeatherInfo); err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"status":  "error",
+					"message": "failed to parse request from weather api",
+				})
+				return
+			}
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"status":  "error",
+				"message": "got an error response from weather api",
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"client_ip": ip,
+			"location":  ipLocInfo.City,
+			"greeting":  fmt.Sprintf("Hello, %s!, the temperature is %s degrees Celcius in %s", visitor_name, ipWeatherInfo.Main.Temp, ipLocInfo.City),
+		})
 	})
 
 	// exec router
